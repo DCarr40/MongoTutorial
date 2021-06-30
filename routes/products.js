244f921ef3aee571f62b976d4de6096b389e6ea9
DCarr40 <davidwcarruthers@yahoo.com>
2021-06-29 20:24:52 -0400

    rewrote some lines to see if it fixed code

--- routes/products.js
+++ routes/products.js
@@ -4,20 +4,19 @@ const router = express.Router();
 
 //All end points and routes handlers go here
 router.post('/', async (req, res) => {
-    try {
-   
-    const product = new Product({
-    name: 'Stanley Classic Vacuum Bottle',
-    description: `Our Stanley Classic Vacuum Bottle is made with superior
-   insulation that keeps liquids (soup, coffee, tea) hot or cold drinks cool for up
-   to 24 hours.`,
-    category: 'Travel',
-    price: 19.82,
-    });
-    await product.save();
-    return res.send(product);
-    } catch (ex) {
-    return res.status(500).send(`Internal Server Error: ${ex}`);
+    try{
+        const product = new Product({
+            name: 'Stanley Classic Vacuum Bottle',
+            description: 'Our Stanley Classic Vacuum Bottle is made with superior insulation that keeps liquids (soup, coffee, tea) hot or cold drinks for up to 24hrs.',
+            category: 'Travel',
+            price: 19.82,
+        })
+        await product.save()
+
+        return res.send(product);
+
+    } catch(ex){
+        return res.status(500).send(`Internal Server Error: ${ex}`);
     }
-   });
-module.exports = router;
+});
+
